0cffff920439b366d16165705895d14671decfc1
ProjectPixel <jasonflor.cenpri@gmail.com>
2019-08-22 10:12:46 +0800

    upload file

--- application/views/masterfile/report_list.php
+++ application/views/masterfile/report_list.php
@@ -1,75 +1,142 @@
-<div class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 col-lg-12 col-sm-12">
-                <div class="card">
-                    <div class="header">
-                        <h4 class="title">CHECK VOUCHER</h4>
-                        <p class="category">Here is a subtitle for this table</p>
-                        <br>
-                    </div>
-                	<div class="datatable-dashv1-list custom-datatable-overright">
-                        <table class="table table-hover" id="myTable">
-                            <thead>
-                                <th width="15%">Date</th>
-                            	<th width="50%">Payee</th>
-                            	<th width="20%">Number</th>
-                            	<th width="10%">Status</th>
-                            	<th width="5%" align="center"><span class="ti-menu"></span></th>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                	<td>1</td>
-                                	<td>Dakota Rice</td>
-                                	<td>$36,738</td>
-                                	<td>Niger</td>
-                                	<td align="center">
-                                		<a href="<?php echo base_url(); ?>index.php/masterfile/form" class="btn btn-xs btn-warning btn-fill">
-                                			<span class="ti-eye"></span>
-                                		</a>
-                                	</td>
-                                </tr>
-                                <tr>
-                                	<td>1</td>
-                                	<td>Dakota Rice</td>
-                                	<td>$36,738</td>
-                                	<td>Niger</td>
-                                	<td align="center">
-                                		<a href="<?php echo base_url(); ?>index.php/masterfile/form" class="btn btn-xs btn-warning btn-fill">
-                                			<span class="ti-eye"></span>
-                                		</a>
-                                	</td>
-                                </tr>
-                                <tr>
-                                	<td>1</td>
-                                	<td>Dakota Rice</td>
-                                	<td>$36,738</td>
-                                	<td>Niger</td>
-                                	<td align="center">
-                                		<a href="<?php echo base_url(); ?>index.php/masterfile/form" class="btn btn-xs btn-warning btn-fill">
-                                			<span class="ti-eye"></span>
-                                		</a>
-                                	</td>
-                                </tr>
-                                <tr>
-                                	<td>1</td>
-                                	<td>Dakota Rice</td>
-                                	<td>$36,738</td>
-                                	<td>Niger</td>
-                                	<td align="center">
-                                		<a href="<?php echo base_url(); ?>index.php/masterfile/form" class="btn btn-xs btn-warning btn-fill">
-                                			<span class="ti-eye"></span>
-                                		</a>
-                                	</td>
-                                </tr>
-                            </tbody>
-                        </table>
+<div class="modal fade" id="uploadfile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Upload File
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </h5>                                        
+            </div>
+            <form>
+                <div class="modal-body">
+                    <input type="file" class="form-control" name="">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary btn-block">Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="main-panel">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar bar1"></span>
+                    <span class="icon-bar bar2"></span>
+                    <span class="icon-bar bar3"></span>
+                </button>
+                <a href="javascript:history.go(-1)" class="btn btn-success btn-md p-l-100 p-r-100"><span class="ti-arrow-left"></span> Back</a>            
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="ti-panel"></i>
+                            <p>Stats</p>
+                        </a>
+                    </li>
+                    <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="ti-settings"></i>
+                                <p>Settings</p>
+                                <b class="caret"></b>
+                          </a>
+                          <ul class="dropdown-menu">
+                            <li><a href="#">Logout</a></li>
+                          </ul>
+                    </li>
+                    <!-- <li>
+                        <a href="#">
+                            <i class="ti-settings"></i>
+                            <p>Settings</p>
+                        </a>
+                    </li> -->
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 col-lg-12 col-sm-12">
+                    <div class="card">
+                        <div class="header">
+                            <h4 class="title">CHECK VOUCHER
+                                <button type="button" class="btn btn-primary btn-fill pull-right" data-toggle="modal" data-target="#uploadfile">
+                                  <span class="ti-printer"></span> Upload File
+                                </button>                            
+                            </h4>
+                            <p class="category">Here is a subtitle for this table</p>
+
+                            <br>
+                        </div>
+                    	<div class="datatable-dashv1-list custom-datatable-overright">
+                            <table class="table table-hover" id="myTable">
+                                <thead>
+                                    <th width="15%">Date</th>
+                                	<th width="50%">Payee</th>
+                                	<th width="20%">Number</th>
+                                	<th width="10%">Status</th>
+                                	<th width="5%" align="center"><span class="ti-menu"></span></th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                    	<td>1</td>
+                                    	<td>Dakota Rice</td>
+                                    	<td>$36,738</td>
+                                    	<td>Niger</td>
+                                    	<td align="center">
+                                    		<a href="<?php echo base_url(); ?>index.php/masterfile/form" class="btn btn-xs btn-warning btn-fill">
+                                    			<span class="ti-eye"></span>
+                                    		</a>
+                                    	</td>
+                                    </tr>
+                                    <tr>
+                                    	<td>1</td>
+                                    	<td>Dakota Rice</td>
+                                    	<td>$36,738</td>
+                                    	<td>Niger</td>
+                                    	<td align="center">
+                                    		<a href="<?php echo base_url(); ?>index.php/masterfile/form" class="btn btn-xs btn-warning btn-fill">
+                                    			<span class="ti-eye"></span>
+                                    		</a>
+                                    	</td>
+                                    </tr>
+                                    <tr>
+                                    	<td>1</td>
+                                    	<td>Dakota Rice</td>
+                                    	<td>$36,738</td>
+                                    	<td>Niger</td>
+                                    	<td align="center">
+                                    		<a href="<?php echo base_url(); ?>index.php/masterfile/form" class="btn btn-xs btn-warning btn-fill">
+                                    			<span class="ti-eye"></span>
+                                    		</a>
+                                    	</td>
+                                    </tr>
+                                    <tr>
+                                    	<td>1</td>
+                                    	<td>Dakota Rice</td>
+                                    	<td>$36,738</td>
+                                    	<td>Niger</td>
+                                    	<td align="center">
+                                    		<a href="<?php echo base_url(); ?>index.php/masterfile/form" class="btn btn-xs btn-warning btn-fill">
+                                    			<span class="ti-eye"></span>
+                                    		</a>
+                                    	</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
         
--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -14,7 +14,7 @@
 
             <ul class="nav">
                 <li class="">
-                    <a href="<?php echo base_url(); ?>index.php/masterfile/dashboard">
+                    <a href="<?php echo base_url(); ?>index.php/masterfile/index">
                         <i class="ti-panel"></i>
                         <p>Dashboard</p>
                     </a>
@@ -39,46 +39,4 @@
                 </li>
             </ul>
     	</div>
-    </div>
-
-    <div class="main-panel">
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar bar1"></span>
-                        <span class="icon-bar bar2"></span>
-                        <span class="icon-bar bar3"></span>
-                    </button>
-                    <a href="javascript:history.go(-1)" class="btn btn-success btn-md p-l-100 p-r-100"><span class="ti-arrow-left"></span> Back</a>            
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="ti-panel"></i>
-								<p>Stats</p>
-                            </a>
-                        </li>
-                        <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="ti-settings"></i>
-									<p>Settings</p>
-									<b class="caret"></b>
-                              </a>
-                              <ul class="dropdown-menu">
-                                <li><a href="#">Logout</a></li>
-                              </ul>
-                        </li>
-						<!-- <li>
-                            <a href="#">
-								<i class="ti-settings"></i>
-								<p>Settings</p>
-                            </a>
-                        </li> -->
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
+    </div>    
